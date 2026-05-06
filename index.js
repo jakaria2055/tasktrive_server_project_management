dd8a31948aa2062ef611a38bea545b2869bb7991
Jakaria Ahmed <jakaria88520@gmail.com>
2026-05-06 17:00:49 +0600

    setup inngest

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest/index.js"
 
 const app = express();
 
@@ -12,6 +14,9 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) =>
   res.send("Hello!!! TaskTrive Server is Running Fine..."),
 );
+
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000;
 
---
+++ src/inngest/index.js
@@ -0,0 +1,72 @@
+import { Inngest } from "inngest";
+import { prisma } from "../db.js";
+
+// Create a client
+export const inngest = new Inngest({
+  id: "project-management",
+});
+
+// CREATE USER
+const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name || ""} ${data?.last_name || ""}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+// DELETE USER
+const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.delete({
+      where: {
+        id: data.id,
+      },
+    });
+  }
+);
+
+// UPDATE USER
+const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
+
+    await prisma.user.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        email: data?.email_addresses[0]?.email_address,
+        name: `${data?.first_name || ""} ${data?.last_name || ""}`,
+        image: data?.image_url,
+      },
+    });
+  }
+);
+
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
